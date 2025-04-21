1c933c90ab5af77424cf7edc34a47ffafaf0662c
Ryan Johnson <137436926+RyanSR71@users.noreply.github.com>
2025-04-21 03:03:46 -0700

    Update ppE.py

--- ppE.py
+++ ppE.py
@@ -1,5 +1,5 @@
 "ppE package"
-__version__ = "0.6.0.1"
+__version__ = "0.6.0.2"
 
 import numpy as np
 from numpy.linalg import inv
@@ -190,9 +190,7 @@ class WaveformGeneratorPPE(object):
         beta = inversion_function(0,3,parameters['b'])*beta_from_beta_tilde_wrapped(beta_tilde,self.waveform_arguments['f_low'],1/np.pi,parameters['b'],0.018,total_mass)
                               
         delta_epsilon_tilde = parameters['delta_epsilon_tilde']
-        delta_epsilon = -10*delta_epsilon_tilde*(parameters['b']-3)
-
-        print(beta,delta_epsilon)
+        delta_epsilon = delta_epsilon_tilde
                               
         model_strain['plus'] = apply_ppe_correction(model_strain['plus'],self.frequency_array,total_mass,beta,parameters['b'],delta_epsilon,0.018,0.5,True)
         model_strain['cross'] = apply_ppe_correction(model_strain['cross'],self.frequency_array,total_mass,beta,parameters['b'],delta_epsilon,0.018,0.5,True)
